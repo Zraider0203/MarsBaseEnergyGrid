5f7709e5505878ead0803f2a22654ab4e3bb93b8
Zraider0203 <144168449+Zraider0203@users.noreply.github.com>
2024-01-24 16:37:37 -0700

    First Upload

---
+++ MarsBaseEnergyGrid.java
@@ -0,0 +1,11 @@
+// Create the main class for the Mars Base Energy Grid for this assignmnt
+public class MarsBaseEnergyGrid{
+    // Create the starting point of the program which will always be the main method
+    public static void main(string[] args){
+        // Display a title to our user
+        System.out.println("\n\n*** Welcome to the Mars Base Energy Grid ***\n\n");
+
+        // Say goodbye to our user
+        System.out.println("\n\n*** Thank you for using the Mars Base Energy Grid *** \n\n");
+    }
+}
